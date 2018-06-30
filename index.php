c3cb41750c81c829d75dbc2a145dae2ecf818901
nyzvin <vinicius.sg2002@gmail.com>
2018-06-30 19:44:24 -0300

    Revert "Revert "Primeiro website upado""
    This reverts commit 40d84552d49c5ee55d4ddc8fa8782a5f11633810.

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<title> Lixo Interdisciplinar </title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="css/uikit.min.css" />
+		<script src="js/uikit.min.js"></script>
+		<script src="js/uikit-icons.min.js"></script>
+
+	</head>
+	<body>
+
+		<!-- <nav class="uk-navbar-container" uk-navbar>
+			<div class="uk-navbar-center">
+				<ul class="uk-navbar-nav">
+					<li><a href="#">VINIZAO</a></li>
+					<li><a href="#">GUI</a></li>
+					<li><a href="#">GUTO</a></li>
+					<li><a href="#">MARRETA</a></li>
+				</ul>
+			</div>
+		</nav> -->
+
+		<div class="uk-position-relative">
+			<header id="indexHeader" class="uk-grid uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light">
+				<h1 id="text">devianART</h1>
+			</header>
+			<div class="uk-position-top">
+				<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+					<div class="uk-navbar-center">
+						<ul class="uk-navbar-nav">
+							<li><a href="#">VINIZAO</a></li>
+							<li><a href="#">GUIZADINHA</a></li>
+							<li><a href="#">GUTOGANG</a></li>
+							<li><a href="#">MARRETA</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+		
+		<div class="uk-container-expand uk-background-secondary">
+			<div class="uk-container uk-container-center uk-light">
+				<br> <br>
+
+				<h2 class="uk-text-center uk-h2"> Sobre </h2>
+
+				<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				<br> <br> <br>
+			</div>
+		</div>
+
+		<div class="uk-height-medium uk-background-cover uk-light uk-flex" uk-parallax="bgy: -130" style="background-image: url('imgs/img1.jpg');">
+			<h2 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">DESEJA PARTICIPAR DESSE UNIVERSO COM A GENTE?</h2>
+		</div>
+
+		<div class="uk-container-expand">
+			<div class="uk-container uk-container-center uk-dark">
+				<br> <br>
+
+				<h2 class="uk-h2 uk-text-center">Cadastre-se</h2>
+				<p class="uk-muted uk-text-center">É rápido, fácil e ainda abre milhares de portas!</p>
+				<br>
+
+				<form class="uk-text-center" method="POST">
+					<div class="uk-margin">
+						<input class="uk-input uk-form-width-large" type="text" placeholder="Nome e Sobrenome" required>
+					</div>
+					<div class="uk-margin">
+						<input class="uk-input uk-form-width-large" type="text" placeholder="Nickname" required>
+					</div>
+					<div class="uk-margin">
+						<input class="uk-input uk-form-width-large" type="email" placeholder="Email" required>
+					</div>
+					<div class="uk-margin">
+						<input class="uk-input uk-form-width-large" type="password" placeholder="Senha" required>
+					</div>
+					<div class="uk-margin">
+						<input class="uk-input uk-form-width-large" type="password" placeholder="Confirmação de Senha" required>
+					</div>
+					<button class="uk-button uk-button-secondary" type="submit">ENVIAR</button>
+				</form>
+
+				<br> <br> <br> <br>
+			</div>
+		</div>
+
+		<div class="uk-container-expand uk-background-muted">
+			<div class="uk-container uk-container-center uk-dark">
+				<br> <br>
+
+				<h2 class="uk-h2 uk-text-center">Desenvolvedores</h2>
+				<p class="uk-muted uk-text-center">Todos que contribuiram para que isto se tornasse realidade!</p>
+				<br> <br>
+
+				<div class="uk-grid" data-uk-grid-margin>
+					<div class="uk-width-1-1@s uk-width-1-4@m">
+						<h2 class="uk-h2 uk-text-center">Vini</h2>
+						<img src="imgs/img4.png" class="uk-border-rounded uk-height-small uk-align-center">
+						<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>	
+					</div>
+					<div class="uk-width-1-1@s uk-width-1-4@m">
+						<h2 class="uk-h2 uk-text-center">Gui</h2>
+						<img src="imgs/img4.png" class="uk-border-rounded uk-height-small uk-align-center">
+						<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>	
+					</div>
+					<div class="uk-width-1-1@s uk-width-1-4@m">
+						<h2 class="uk-h2 uk-text-center">Guto</h2>
+						<img src="imgs/img4.png" class="uk-border-rounded uk-height-small uk-align-center">
+						<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>	
+					</div>
+					<div class="uk-width-1-1@s uk-width-1-4@m">
+						<h2 class="uk-h2 uk-text-center">Marreta</h2>
+						<img src="imgs/img4.png" class="uk-border-rounded uk-height-small uk-align-center">
+						<p class="uk-text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</p>	
+					</div>
+				</div>
+
+				<br> <br> <br> <br>
+			</div>
+				
+		</div>
+
+		<div class="uk-height-medium uk-background-cover uk-light uk-flex" uk-parallax="bgy: -130" style="background-image: url('imgs/img2.jpg');">
+			<h2 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">CADASTRE-SE AGORA E ADENTRE TODO ESSE NOVO UNIVERSO!</h2>
+		</div>
+
+		<div class="uk-card-footer uk-light uk-background-secondary uk-text-center" style="bottom: 0;">
+			© 2018 Copyright: <a href="#" class="text-white">Vinícius Gomes</a>, <a href="#" class="text-white">Guilherme Ribeiro</a>, <a href="#" class="text-white">Augusto Guilarducci</a> e <a href="#" class="text-white">Felipe Henrique</a>
+		</div>
+
+	</body>
+</html>
